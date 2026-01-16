c1c0a37b6f191315d2b6857352c86bbe646be963
Gaurav81783892 <gauravkumar9540585938@gmail.com>
2026-01-16 16:27:16 +0530

    Update evidenceScorer.js

--- brain/evidenceScorer.js
+++ brain/evidenceScorer.js
@@ -1,1 +1,17 @@
+export function scoreEvidence(symptoms, medicalDB) {
+  const scores = {};
+
+  symptoms.forEach(symptom => {
+    medicalDB.forEach(disease => {
+      if (disease.symptoms.includes(symptom)) {
+        if (!scores[disease.name]) {
+          scores[disease.name] = 0;
+        }
+        scores[disease.name] += disease.weight || 1;
+      }
+    });
+  });
+
+  return scores;
+}
 
